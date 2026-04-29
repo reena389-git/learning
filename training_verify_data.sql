b471720af9fd161c1a9287cf499913b84cdc1306
reena389-git <81640752+reena389-git@users.noreply.github.com>
2026-04-29 07:32:24 -0400

    Update training_verify_data.sql

--- training_verify_data.sql
+++ training_verify_data.sql
@@ -3,3 +3,10 @@ SELECT trade_id, trade_date, booked_name,
 FROM `d4001-centralus-tdvip-creditrisk`.xvala_xva.training_fact_trades
 WHERE resolution_status = 'UNMATCHED'
 ORDER BY trade_date;
+
+
+--- verify 2 , after resetting data
+SELECT resolution_status, COUNT(*) AS trades, 
+       SUM(exposure_amount) AS exposure
+FROM `d4001-centralus-tdvip-creditrisk`.xvala_xva.training_fact_trades
+GROUP BY resolution_status;
